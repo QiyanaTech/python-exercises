eddf9671a44ace569bb356a47b09bc0264c71f9c
Qiyana Tech <qiyanatech@gmail.com>
2025-01-08 10:49:10 -0300

    add the real symbol

--- Exercise number 9.py
+++ Exercise number 9.py
@@ -1,6 +1,6 @@
 price = float(input("Enter the price of your purchase to find out your discount: "))
 
-def discount():
+def discount(price):
     price_discount = 0
     if price < 100:
         final_price = price - price * price_discount/100
@@ -8,14 +8,14 @@ def discount():
     elif price >= 100 and price < 200:
         price_discount = 10
         final_price = price - price * price_discount/100
-        print(f"Your discount is {price_discount}%. The price of your purchase is {final_price}.")
+        print(f"Your discount is {price_discount}%. The price of your purchase is R${final_price}.")
     elif price >= 200 and price < 500:
         price_discount = 15
         final_price = price - price * price_discount/100
-        print(f"Your discount is {price_discount}%. The price of your purchase is {final_price}.")
+        print(f"Your discount is {price_discount}%. The price of your purchase is R${final_price}.")
     else:
         price_discount = 25
         final_price = price - price * price_discount/100
-        print(f"Your discount is {price_discount}%. The price of your purchase is {final_price}.")
+        print(f"Your discount is {price_discount}%. The price of your purchase is R${final_price}.")
 
-discount()
+discount(price)
